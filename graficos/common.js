916723b4bb62a236f9a99907150575b5fb011eb7
stehcarneiro17 <165041630+stehcarneiro17@users.noreply.github.com>
2024-11-05 19:04:29 -0300

    .

--- graficos/common.js
+++ graficos/common.js
@@ -1,12 +1,12 @@
 const getCSS = (variavel) => {
     const bodyStyles = getComputedStyle(document.body)
     return bodyStyles.getPropertyValue(variavel)
-}
-
-const tickConfig = {
+    }
+    
+    const tickConfig = {
     family: getCSS('--font'),
     size: 16,
     color: getCSS('--primary-color')
-}
-
-export {getCSS, tickConfig}
+    }
+    
+    export {getCSS, tickConfig}
